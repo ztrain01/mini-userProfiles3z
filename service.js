ddf1a55f15ae35fbae619faabbc413df716d31e7
Zander Allen <ztrain01@gmail.com>
2015-02-17 19:13:39 -0700

    added a promise to my service

--- service.js
+++ service.js
@@ -2,11 +2,14 @@ var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
 
-
+  var deffered = $q.defer();
   this.getUsers = function() {
     return $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response) {
+      deffered.resolve(response)
     })
+    return deffered.promise;
   }
 });
