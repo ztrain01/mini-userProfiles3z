8ffaf66c9d56127ffd979cbcfce6067b46630aa8
Zander Allen <ztrain01@gmail.com>
2015-02-17 19:25:40 -0700

    finished mini project

--- service.js
+++ service.js
@@ -4,11 +4,14 @@ app.service('mainService', function($http, $q) {
 
   var deffered = $q.defer();
   this.getUsers = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
     }).then(function(response) {
       response = response.data.data
+      for(var i = 0; i < response.length; i++) {
+        response[i].first_name = 'Ralf'
+      }
       deffered.resolve(response)
     })
     return deffered.promise;
